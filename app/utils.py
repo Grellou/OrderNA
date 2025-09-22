ca7af16b04841b8eff49632fc1b28565ea3e8908
Grellou <aleknaharoldas@yahoo.com>
2025-09-22 20:41:17 +0300

    Stock: initial stock imported

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,9 @@
+import csv
+
 import click
 
-from app import create_app, db
+from app import db
+from app.models.item_model import ItemModel
 from app.models.user_model import UserModel
 
 
@@ -23,5 +26,25 @@ def create_admin_command(email_address, password):
     click.echo(f"Admin account {email_address} created successfully!")
 
 
+# Import initial stock to db
+@click.command("import-stock")
+@click.argument("filename")
+def import_stock_command(filename):
+    with open(filename, "r", encoding="utf-8-sig") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            item = ItemModel(
+                name=row["Name"].strip(),  # type: ignore
+                category=row["Category"].strip(),  # type: ignore
+                language=row["Language"].strip(),  # type: ignore
+                quantity=int(row["Quantity"] or 0),  # type: ignore
+                price=float(row["Price"]),  # type: ignore
+            )
+            db.session.add(item)
+        db.session.commit()
+        click.echo("Successfully imported stock!")
+
+
 def register_commands(app):
     app.cli.add_command(create_admin_command)
+    app.cli.add_command(import_stock_command)
